40a4fe324cf8a831b1ba80ba6d0cd04476f1205d
IHA Yasushi <yasuc18@gmail.com>
2023-04-28 14:09:44 +0900

    init.luaの修正とvimフォルダの削除

--- init.lua
+++ init.lua
@@ -1,14 +1,3 @@
---vim.g.use_lazy = true
-
---if not vim.g.use_lazy then
---	local ok, impatient = pcall(require, "impatient")
---	if ok then
---		impatient.enable_profile()
---	else
---		vim.notify("cannot load impatient", vim.log.levels.WARN)
---	end
---end
-
 require("base")
 require("autocmds")
 require("options")
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\yasuc\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  ["ayu-vim"] = {
+    loaded = true,
+    path = "C:\\Users\\yasuc\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\ayu-vim",
+    url = "https://github.com/ayu-theme/ayu-vim"
+  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "C:\\Users\\yasuc\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-buffer",
@@ -108,6 +113,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "C:\\Users\\yasuc\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
+  },
+  ["jaq-nvim"] = {
+    loaded = true,
+    path = "C:\\Users\\yasuc\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\jaq-nvim",
+    url = "https://github.com/is0n/jaq-nvim"
   },
   ["lspkind-nvim"] = {
     loaded = true,
